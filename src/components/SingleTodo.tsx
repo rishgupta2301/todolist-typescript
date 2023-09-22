26c39730209160034df2f78ba1ce5a0e9b3a93c0
rishgupta2301 <guptarishabh1122@gmail.com>
2023-09-22 11:00:42 +0530

    added edit functionality

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
@@ -11,32 +11,70 @@ type Props = {
 };
 
 const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }: Props) => {
-    const handleDone = (id:number) => {
-        setTodos(todos.map((todo) => {
-            if(todo.id === id){
-                todo.isDone=!todo.isDone
-            }
-            console.log("clicked")
-            return todo;
-        }))
-    }
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-    const handleDelete=(id:number) => {
-        setTodos(todos.filter((todo) => {
-            return (todo.id !== id)
-        }))
-    }
-  return (
-    <form className="todos__single">
-        {
-            todo.isDone ? (
-                <s className="todos__single--text">{todo.todo}</s>
-            ) : (
-                <span className="todos__single--text">{todo.todo}</span>
-            )
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.isDone = !todo.isDone;
         }
+        console.log("clicked");
+        return todo;
+      })
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => {
+        if(todo.id === id) {
+          return { ...todo, todo: editTodo }
+        } 
+         return todo; 
+      }
+  ))
+      setEdit(!edit);
+};
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {  // using to automatically focus on the text field on clicking the edit button 
+    inputRef.current?.focus();
+  },[edit])
+
+  return (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+        ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todos__single--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
